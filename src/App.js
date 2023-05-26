22c3de871f1d3c42b3bd839e8bc1931ee66b1783
apersonius <personius12@gmail.com>
2023-05-25 21:46:30 -0500

    errors again

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
-import { BrowseRouter as Router, Routes, Route } from 'react-router-dom';
+import { Router, Routes, Route } from 'react-router-dom';
 import Footer from './Footer';
 import About from './AboutMe';
 import Contact from './Contact';
-import Home from './Home';
 import Portfolio from './Portfolio';
 import Resume from './Resume';
 
