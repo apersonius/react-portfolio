0f82af7f4803d3486d5770acec15da1ecb2cfcea
apersonius <personius12@gmail.com>
2023-05-25 20:54:35 -0500

    portfolio projects

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,10 +1,41 @@
 const Portfolio = () => {
 
-    return (
-      <div className="portfolio">
-       <h1>portfolio</h1>
+  return (
+    <div class="portfolio">
+        <h2>Book Search</h2>
+        <a href="https://bc-book-search.herokuapp.com/">
+          <img src='' alt="book search homepage" />
+        </a>
+
+        <h2>Recipes for Disaster</h2>
+        <a href="https://github.com/apersonius/recipes-for-disaster">
+          <img src='' alt="recipes for disaster homepage" />
+        </a>
+
+        <h2>Mixer</h2>
+        <a href="https://paint-mixer.herokuapp.com/">
+          <img src='' alt="mixer homepage" />
+        </a>
+
+        <h2>Social Media App</h2>
+        <a href="https://github.com/apersonius/socialMediaApp">
+          <img src='' alt="social media repo" />
+        </a>
+
+        <h2>E Commerce Backend</h2>
+        <a href="https://github.com/apersonius/eCommBackend">
+          <img src='' alt="e commerce backend repo" />
+        </a>
+
+        <h2>Text Editor</h2>
+        <a href="https://bc-text-editor.herokuapp.com/">
+          <img src='' alt="text editor homepage" />
+        </a>
+
       </div>
-    );
-  }
-   
-  export default Portfolio;
+  );
+}
+ 
+export default Portfolio;
+
+// add images!
