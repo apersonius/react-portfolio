cb0aff6c857a17baa0fbf45750580ffe40adbc6e
apersonius <personius12@gmail.com>
2023-05-25 20:36:57 -0500

    navbar style

--- src/Nav.js
+++ src/Nav.js
@@ -5,11 +5,10 @@ const Navbar = () => {
     <nav className="navbar">
       <h1>Abigail Personius</h1>
       <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/about">About me</Link>
-        <Link to="/portfolio">Portfolio</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/resume">Resume</Link>
+        <Link to="/about"> style={{color: "var(--blue)"}}About me</Link>
+        <Link to="/portfolio" style={{color: "var(--indigo)"}}>Portfolio</Link>
+        <Link to="/contact" style={{color: "var(--teal)"}}>Contact</Link>
+        <Link to="/resume" style={{color: "var(--cyan)"}}>Resume</Link>
       </div>
     </nav>
   );
