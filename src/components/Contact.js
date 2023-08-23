78d5d8258c3f1a34a4abe0fd14c250f87543b338
apersonius <personius12@gmail.com>
2023-08-22 20:06:13 -0500

    add correct location

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,8 +14,7 @@ export default function Contact() {
                     marginHeight={0}
                     marginWidth={0}
                     style={{ filter: "opacify(0.7)" }}
-                    // insert correct info for me
-                    src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"/>
+                    src="https://www.google.com/maps/embed/v1/place?q=Minneapolis,+MN,+USA&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"/>
                 <div className="bg-gray-900 realtive flex flex-wrap py-6 rounded shadow-md">
                     <div className="lg:w-1/2 px-6">
                         <h2 className="title-font font-semibold text-white tracking-widest text-xs">
