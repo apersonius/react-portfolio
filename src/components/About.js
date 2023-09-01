65ba1c714ddc470616eb5e7bee777e5b3865715c
apersonius <personius12@gmail.com>
2023-09-01 13:54:55 -0500

    fix contact form

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,8 @@ export default function About() {
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md_w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                        Hey there! I'm Abigail and I'm thrilled to welcome you to my portfolio
-                        <br className="hidden lg:inline-block" />I'm enjoying building my skills and building apps
+                        Hey there! I'm Abigail
+                        <br className="hidden lg:inline-block" />Welcome to my portfolio
                     </h1>
                     <p className="mb-8 leading-relaxed">
                     With a diverse journey shaping my path, I am a passionate Full Stack Engineer applying my unique background to create innovative solutions. As a former Division 1 soccer player, I bring the tenacity and discipline necessary to tackle complex challenges in the tech world. My six-year immersion in the healthcare sector, specifically in ophthalmology working alongside surgeons, has honed my meticulous attention to detail, teamwork, and ability to thrive in high-pressure environments. These skills seamlessly translate into my approach to coding, where precision and collaboration are paramount. I am a graduate of the University of Minnesota's rigorous Full Stack Web Development Bootcamp, where I not only acquired a solid foundation in web technologies like HTML, CSS, JavaScript, and SQL but also developed a problem-solving mindset that is attuned to user-centric design. My journey, from the soccer field to the operating room, and now into the realm of full stack engineering, reflects my adaptability, resilience, and hunger for continuous growth. Eager to contribute my diverse skills and unwavering dedication to crafting seamless and impactful digital experiences, I am excited to embark on new challenges and leverage technology to make a lasting impact                    </p>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -48,9 +48,10 @@ export default function Contact() {
                         <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                             EMAIL
                         </h2>
-                        <a className="text-indigo-400 leading-relaxed">
+                        <a href="mailto:personius12@gmail.com" className="text-indigo-400 leading-relaxed">personius12@gmail.com</a>
+                        {/* <a className="text-indigo-400 leading-relaxed">
                             personius12@gmail.com
-                        </a>
+                        </a> */}
                         <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                             PHONE
                         </h2>
@@ -59,15 +60,16 @@ export default function Contact() {
                 </div>
                 </div>
                 <form 
-                netlify="true"
+                data-netlify="true"
                 name="contact"
+                onSubmit={handleSubmit}
                 className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                     <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
                         Hire me
                     </h2>
-                    <p className="leading-relaxed mb-5">
+                    {/* <p className="leading-relaxed mb-5">
                         loremmmmmm
-                    </p>
+                    </p> */}
                     <div className="relative mb-4">
                         <label htmlFor="name" className="leading-7 text-sm text-gray-400">
                             Name
@@ -77,7 +79,20 @@ export default function Contact() {
                             id="name"
                             name="name"
                             className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors durations-200 easi-in-out"
+                            onChange={(e) => setName(e.target.value)}
                             />
+                    </div>
+                    <div className="relative mb-4">
+                        <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+                            Email
+                        </label>
+                        <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        onChange={(e) => setEmail(e.target.value)}
+                        />
                     </div>
                     <div className="relative mb-4">
                         <label
@@ -88,7 +103,9 @@ export default function Contact() {
                         <textarea
                         id="message"
                         name="message"
-                        className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"/>
+                        className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                        onChange={(e) => setMessage(e.target.value)}
+                        />
                     </div>
                     <button
                     type="submit"
